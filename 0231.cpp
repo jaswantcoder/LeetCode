ed2172ec4f97ade2f2d984a31dc8bb05547bb383
Jaswant Arya <42512007+jaswantcoder@users.noreply.github.com>
2020-06-09 11:35:27 +0530

    Power of Two

---
+++ 0231.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>1)
+        {
+           if(n&1) return false;
+            else n/=2;
+        }
+        return true;
+        
+
+        
+    }
+};
